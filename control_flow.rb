17e4a7502b66068528e130b7aca264c1b72c4b42
Aris Vayas <aris.vayas@gmail.com>
2022-05-16 11:16:24 -0400

    commit tree 7f394c3b2d32332980794310b9fd124df3c9e483

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,20 @@
 def admin_login(username, password)
-  # your code here
+  if (username.downcase=="admin" && password == 12345)
+     puts "Access granted"
+  else
+    puts "Access denied"
+  end
 end
 
-def hows_the_weather(temperature)
-  # your code here
-end
+# def hows_the_weather(temperature)
+#   # your code here
+# end
 
-def fizzbuzz(num)
-  # your code here
-end
+# def fizzbuzz(num)
+#   # your code here
+# end
 
-def calculator(operation, num1, num2)
-  # your code here
-end
-
+# def calculator(operation, num1, num2)
+#   # your code here
+# end
+admin_login(username, password)
